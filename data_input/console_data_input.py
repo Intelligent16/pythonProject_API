110659c6b3528a3a8d9d94ec492cb1e259e98fb4
Intelligent16 <classic.alex.91@gmail.com>
2022-08-24 21:45:02 +0300

    add feature transfer_to_card

--- data_input/console_data_input.py
+++ data_input/console_data_input.py
@@ -1,5 +1,6 @@
 from main_console_app.data_base import all_numbers_cards, history_transactions, status_success, status_failed
 
+
 def get_avtoriz_data():
     print("Укажите Ваш логин и пароль")
     login, password = input(), input()
@@ -8,6 +9,7 @@ def get_avtoriz_data():
         login, password = input(), input()
 
     return login, password
+
 
 def transfer_to_card():
     transfer_summ = check_transfer_summ()
@@ -28,12 +30,14 @@ def add_transaction_to_history(all_tax, from_card, to_card, transfer_summ, statu
         {"from_card": from_card, "to_card": to_card, "transfer_sum": transfer_summ, "tax": all_tax,
          "status": status})
 
+
 def get_number_card(message):
     while True:
         print(f"Введите номер {message} карты")
         number_card = input()
         if number_card != "" and number_card in all_numbers_cards:
             return number_card
+
 
 def check_transfer_summ():
     while True:
@@ -44,6 +48,7 @@ def check_transfer_summ():
             if transfer_summ > 0:
                 return transfer_summ
 
+
 def tax_of_transfer(transfer_summ):
     tax = 0
     if transfer_summ < 1000:
@@ -53,6 +58,7 @@ def tax_of_transfer(transfer_summ):
     elif transfer_summ >= 10000:
         tax = transfer_summ * 0.01
     return tax
+
 
 def get_history_transactions():
     for i in history_transactions:
